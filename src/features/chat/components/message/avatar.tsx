871de0100327620d50939ada7399c3752bbf986f
Ryan Luong <lumintriet@gmail.com>
2025-06-10 11:14:56 +0700

    Use user's profile pic for avatar.

--- src/features/chat/components/message/avatar.tsx
+++ src/features/chat/components/message/avatar.tsx
@@ -1,21 +1,15 @@
 import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar";
+import { useUser } from "@clerk/clerk-react";
 import { memo } from "react";
 
 // Memoized because MessageBubble (this component's parent) can rerender because message.content changes
-const MessageAvatar = memo(function MessageAvatar({
-  name,
-  imgUrl,
-  imgAlt
-}: {
-  name: string;
-  imgUrl?: string;
-  imgAlt?: string;
-}) {
+const MessageAvatar = memo(function MessageAvatar({ name }: { name: string }) {
+  const { user } = useUser();
   const fallback = name.slice(0, 2).toUpperCase();
 
   return (
     <Avatar>
-      <AvatarImage src={imgUrl} alt={imgAlt} />
+      <AvatarImage src={user?.imageUrl} />
       <AvatarFallback>{fallback}</AvatarFallback>
     </Avatar>
   );
